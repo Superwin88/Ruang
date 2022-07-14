694ec61e75ab903d546e58444ca68e73cc3d587f
Superwin88 <109314254+Superwin88@users.noreply.github.com>
2022-07-15 04:16:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="boot/css/bootstrap.min.css">
+
+    <title>ngajakcoding</title>
+  </head>
+  <body>
+   
+
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" ><img src="logo.png" height="50" width="60" alt=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="info.php">phpinfo<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="phpmyadmin/index.php">PhpMyadmin</a>
+      </li>
+      
+    </ul>
+    
+  </div>
+</nav>
+<div class="my-5">
+<h1><p class="text-info text-center">halo !!! Sobat ngajakcoding</p></h1>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="boot/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="boot/js/popper.min.js"></script>
+    <script src="boot/js/bootstrap.min.js"></script>
+  </body>
+</html>
